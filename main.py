7b809cceb4279c5022f523f2f8367b76b53f3c10
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2022-03-04 08:25:14 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# Import Libraries
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
+from kivymd.toast import toast
+
+
+# Create a random name class that should inherit from App
+class MyToastMessageApp(MDApp):
+    # Create build method
+    def build(self):
+        # Create a Button
+        btn=MDRaisedButton(text="Show toast message",
+                           font_size=32,
+                           pos_hint={"center_x":0.5, "center_y":0.5})
+
+        # bind the button when the buttn clicked will run showToast method
+        btn.bind(on_release=self.showToast)
+        # return the button
+        return btn
+
+    def showToast(self, instance):
+        # show toast message
+        toast(text="This is a toast message")
+
+
+
+# Create class object
+MyToastMessageApp().run()
